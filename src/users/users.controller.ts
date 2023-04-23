649b5f8e8c79290756066e87b1180fb4afb8d6c3
ilkinm03 <ilkinm2003@gmail.com>
2023-04-23 15:14:06 +0400

    feat: create user model

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Post } from "@nestjs/common";
 
 @Controller("/users")
 export class UsersController {
   @Get()
   public findAllUsers() {
     return "users";
+  }
+
+  @Post()
+  public async createUser(): Promise {
+
   }
 }

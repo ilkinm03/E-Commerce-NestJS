593ad71cc959d6d11058f3ed643a6a7489a0c170
ilkinm03 <ilkinm2003@gmail.com>
2023-04-23 15:29:35 +0400

    chore: globally export prisma module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,15 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from "@nestjs/common";
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from "./prisma/prisma.module";
+import { PrismaService } from "./prisma/prisma.service";
 import { UsersModule } from "./users/users.module";
 
+@Global()
 @Module({
   imports: [PrismaModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
+  exports: [PrismaModule],
 })
 export class AppModule {}

0e0a82fba4272a69992416db6d350df72ee738e5
ilkinm03 <ilkinm2003@gmail.com>
2023-04-24 13:11:20 +0400

    feat: apply global validation pipe

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
-import { Global, Module } from "@nestjs/common";
-import { APP_FILTER } from "@nestjs/core";
+import { Global, Module, ValidationPipe } from "@nestjs/common";
+import { APP_FILTER, APP_PIPE } from "@nestjs/core";
 import { PrismaClientExceptionFilter } from "../dist/common/filters";
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
 import { PrismaModule } from "./prisma/prisma.module";
 import { PrismaService } from "./prisma/prisma.service";
 import { UsersModule } from "./users/users.module";
@@ -16,8 +16,15 @@ import { UsersModule } from "./users/users.module";
     {
       provide: APP_FILTER,
       useClass: PrismaClientExceptionFilter,
-    }
+    },
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        whitelist: true,
+      }),
+    },
   ],
   exports: [PrismaModule],
 })
-export class AppModule {}
+export class AppModule {
+}

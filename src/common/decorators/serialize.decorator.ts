816ff352fbf4082813310be107723d681ebb374e
ilkinm03 <ilkinm2003@gmail.com>
2023-04-24 16:04:56 +0400

    fix: typo in interceptor folder name

--- src/common/decorators/serialize.decorator.ts
+++ src/common/decorators/serialize.decorator.ts
@@ -1,6 +1,6 @@
 import { UseInterceptors } from "@nestjs/common";
 import { ClassConstructor } from "class-transformer";
-import { SerializeInterceptor } from "../inteceptors";
+import { SerializeInterceptor } from "../interceptors";
 
 export function Serialize(dto: ClassConstructor<any>): MethodDecorator & ClassDecorator {
   return UseInterceptors(new SerializeInterceptor(dto));

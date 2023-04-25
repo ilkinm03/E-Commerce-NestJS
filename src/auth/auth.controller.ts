8d5a8769aeb92da63dc4020700e096235022b0c9
ilkinm03 <ilkinm2003@gmail.com>
2023-04-25 10:55:05 +0400

    feat: create mock signup method

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,17 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, HttpCode, HttpStatus, Post } from "@nestjs/common";
+import { User } from "@prisma/client";
 import { AuthService } from "./auth.service";
+import { SignupDto } from "./dtos";
 
 @Controller("auth")
 export class AuthController {
   constructor(
     private readonly authService: AuthService,
   ) {}
+
+  @Post("/local/signup")
+  @HttpCode(HttpStatus.CREATED)
+  public async signup(@Body() signupDto: SignupDto): Promise<User> {
+    return this.authService.signup(signupDto);
+  }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,29 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable } from "@nestjs/common";
+import { User } from "@prisma/client";
+import * as bcrypt from "bcrypt";
+import { UsersService } from "../users/users.service";
+import { SignupDto } from "./dtos";
 
 @Injectable()
 export class AuthService {
+  constructor(
+    private readonly usersService: UsersService,
+  ) {}
+
+  public async signup(signupDto: SignupDto): Promise<User> {
+    const {
+      password,
+      confirm_password,
+      ...userData
+    } = signupDto;
+    if (password !== confirm_password) {
+      throw new BadRequestException("passwords not match");
+    }
+    const hashedPassword: string = await bcrypt.hash(password, 10);
+    const user: User = await this.usersService.create({
+      password: hashedPassword,
+      ...userData,
+    });
+    return user;
+  }
 }

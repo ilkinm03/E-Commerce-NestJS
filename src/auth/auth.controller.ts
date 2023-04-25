96e7ec4d9bc0cac22dc0d1e9952ede1d80a4bd8a
ilkinm03 <ilkinm2003@gmail.com>
2023-04-25 11:38:17 +0400

    fix: return type of auth signup controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from "@nestjs/common";
-import { User } from "@prisma/client";
 import { AuthService } from "./auth.service";
 import { SignupDto } from "./dtos";
+import { ITokens } from "./interfaces";
 
 @Controller("auth")
 export class AuthController {
@@ -11,7 +11,7 @@ export class AuthController {
 
   @Post("/local/signup")
   @HttpCode(HttpStatus.CREATED)
-  public async signup(@Body() signupDto: SignupDto): Promise<User> {
+  public async signup(@Body() signupDto: SignupDto): Promise<ITokens> {
     return this.authService.signup(signupDto);
   }
 }

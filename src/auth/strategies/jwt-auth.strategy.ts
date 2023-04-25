ac6b359fc815dfbc393d1eeaa545169c806dbe2d
ilkinm03 <ilkinm2003@gmail.com>
2023-04-25 15:00:31 +0400

    refactor(strategy): check if the user has refresh token

--- src/auth/strategies/jwt-auth.strategy.ts
+++ src/auth/strategies/jwt-auth.strategy.ts
@@ -1,19 +1,28 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport";
+import { User } from "@prisma/client";
 import { ExtractJwt, Strategy } from "passport-jwt";
 import { ConfigService } from "../../config/config.service";
+import { UsersService } from "../../users/users.service";
 import { TokenPayload } from "../interfaces";
 
 @Injectable()
 export class JwtAuthStrategy extends PassportStrategy(Strategy, "jwt") {
-  constructor(private readonly configService: ConfigService) {
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly usersService: UsersService,
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: configService.jwtAccessTokenSecret,
     });
   }
 
-  public validate(payload: TokenPayload): TokenPayload {
+  public async validate(payload: TokenPayload): Promise<TokenPayload> {
+    const user: User = await this.usersService.user(payload.sub);
+    if (!user.refresh_token) {
+      throw new UnauthorizedException();
+    }
     return payload;
   }
 }

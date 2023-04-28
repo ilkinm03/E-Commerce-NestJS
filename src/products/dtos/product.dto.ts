9087887ad988fc20aead59e8c2e36a17fd2d35f9
ilkinm03 <ilkinm2003@gmail.com>
2023-04-28 10:59:41 +0400

    feat(dtos): create product dto

---
+++ src/products/dtos/product.dto.ts
@@ -0,0 +1,15 @@
+import { Expose } from "class-transformer";
+
+export class ProductDto {
+  @Expose()
+  id: number;
+
+  @Expose()
+  title: string;
+
+  @Expose()
+  description: string;
+
+  @Expose()
+  price: number;
+}

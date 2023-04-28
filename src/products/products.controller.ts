cf34cc4aad994e696931a286608273fd72e7e3dd
ilkinm03 <ilkinm2003@gmail.com>
2023-04-28 10:50:37 +0400

    feat: add create product method

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,6 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
+import { Product } from "@prisma/client";
+import { CreateProductDto } from "./dtos";
 import { ProductsService } from "./products.service";
 
 @Controller("products")
@@ -6,4 +8,9 @@ export class ProductsController {
   constructor(
     private readonly productsService: ProductsService,
   ) {}
+
+  @Post()
+  public async createProduct(@Body() productDto: CreateProductDto): Promise<Product> {
+    return this.productsService.create(productDto);
+  }
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from "@nestjs/common";
+import { Product } from "@prisma/client";
 import { PrismaService } from "../prisma/prisma.service";
+import { CreateProductDto } from "./dtos";
 
 @Injectable()
 export class ProductsService {
   constructor(
     private readonly prismaService: PrismaService,
   ) {}
+
+  public async create(productDto: CreateProductDto): Promise<Product> {
+    return this.prismaService.product.create({
+      data: productDto,
+    });
+  }
 }

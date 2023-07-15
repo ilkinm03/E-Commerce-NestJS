adeff0720662968c4a6a693d77231a2567c88470
ilkinm03 <ilkinm2003@gmail.com>
2023-07-15 12:36:53 +0400

    feat: create permissions enum

---
+++ src/common/enums/permissions.enums.ts
@@ -0,0 +1,10 @@
+export enum PermissionsEnum {
+  USERS_READ = "read:user",
+  USERS_WRITE = "write:user",
+
+  PRODUCTS_READ = "read:product",
+  PRODUCTS_WRITE = "write:product",
+
+  ORDERS_READ = "read:order",
+  ORDERS_WRITE = "write:order",
+}

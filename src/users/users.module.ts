de85ac7b0ac1f9640b1d22856e038b70f59a7aad
ilkinm03 <ilkinm2003@gmail.com>
2023-07-16 12:19:41 +0400

    refactor: import users module in necessary modules

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,11 +1,13 @@
 import { Module } from "@nestjs/common";
 import { PrismaService } from "../prisma/prisma.service";
+import { RolesService } from "./roles/roles.service";
 import { UsersController } from "./users.controller";
 import { UsersService } from "./users.service";
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService, PrismaService],
+  providers: [UsersService, RolesService, PrismaService],
+  exports: [UsersService, RolesService]
 })
 export class UsersModule {
 }

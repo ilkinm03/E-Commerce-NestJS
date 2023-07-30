64fe5c4c60efa553bac892d90237c219e65ebbc8
Ilkin Mammadli <ilkinmammadli01@gmail.com>
2023-07-30 17:57:03 +0400

    feat: create roles dto

--- src/roles/dtos/index.ts
+++ src/roles/dtos/index.ts
@@ -1,3 +1,4 @@
 export * from "./create-role.dto";
 export * from "./add-permission.dto";
 export * from "./remove-permission.dto";
+export * from "./roles.dto";
---
+++ src/roles/dtos/roles.dto.ts
@@ -0,0 +1,19 @@
+import { Expose, Transform } from "class-transformer";
+import { PermissionsDto } from "../../permissions/dtos";
+
+export class RolesDto {
+  @Expose()
+  id: number;
+
+  @Expose()
+  title: string;
+
+  @Expose()
+  @Transform(({ value }) => value.map(p => (
+    {
+      id: p.permission.id,
+      title: p.permission.title,
+    }
+  )))
+  permisisons: PermissionsDto[];
+}

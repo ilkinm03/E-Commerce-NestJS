5b66aa0523d9c166a2f85cac7d348e3ea9ee7e14
Ilkin Mammadli <ilkinmammadli01@gmail.com>
2023-08-15 14:47:16 +0400

    feat(docs): add docs for user controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,16 +7,24 @@ import {
   Post,
   Query, UseGuards,
 } from "@nestjs/common";
+import {
+  ApiBody, ApiConflictResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse, ApiParam,
+  ApiQuery,
+  ApiTags,
+} from "@nestjs/swagger";
 import { User } from "@prisma/client";
 import { Serialize } from "../common/decorators";
 import { JwtAuthGuard } from "../common/guards";
 import {
   CreateUserDto,
   UpdateUserDto,
-  UserDto
+  UserDto,
 } from "./dtos";
 import { UsersService } from "./users.service";
 
+@ApiTags("users")
 @Controller("/users")
 @Serialize(UserDto)
 @UseGuards(JwtAuthGuard)
@@ -25,11 +33,32 @@ export class UsersController {
     private readonly usersService: UsersService,
   ) {}
 
+  @ApiOkResponse({
+    description: "Returns all the users",
+    isArray: true,
+    type: UserDto,
+  })
+  @ApiQuery({
+    description: "Email of the user",
+    name: "email",
+    required: false,
+  })
   @Get()
   public async findAllUsers(@Query("email") email?: string): Promise<User[]> {
     return this.usersService.users(email);
   }
 
+  @ApiOkResponse({
+    description: "Returns a user with the provided id",
+    type: UserDto,
+  })
+  @ApiNotFoundResponse({
+    description: "User with the provided id not found",
+  })
+  @ApiParam({
+    description: "Id of the user",
+    name: "id",
+  })
   @Get(":id")
   public async findUserById(@Param(
     "id",
@@ -38,11 +67,34 @@ export class UsersController {
     return this.usersService.user(id);
   }
 
+  @ApiOkResponse({
+    description: "Creates and returns the user",
+    type: UserDto,
+  })
+  @ApiConflictResponse({
+    description: "User with the provided email already exists",
+  })
+  @ApiBody({
+    type: CreateUserDto,
+  })
   @Post()
   public async createUser(@Body() userDto: CreateUserDto): Promise<User> {
     return this.usersService.create(userDto);
   }
 
+  @ApiOkResponse({
+    description: "Updates and returns the updated user",
+    type: UserDto,
+  })
+  @ApiNotFoundResponse({
+    description: "User with the provided id not found"
+  })
+  @ApiConflictResponse({
+    description: "User with the provided email already exists",
+  })
+  @ApiBody({
+    type: UpdateUserDto,
+  })
   @Patch(":id")
   @HttpCode(HttpStatus.OK)
   public async updateUser(
@@ -52,6 +104,17 @@ export class UsersController {
     return this.usersService.update(id, userDto);
   }
 
+  @ApiOkResponse({
+    description: "Deletes a user with the provided id",
+    type: UserDto,
+  })
+  @ApiNotFoundResponse({
+    description: "User with the provided id not found",
+  })
+  @ApiParam({
+    description: "Id of the user",
+    name: "id",
+  })
   @Delete(":id")
   @HttpCode(HttpStatus.OK)
   public async deleteUser(@Param(

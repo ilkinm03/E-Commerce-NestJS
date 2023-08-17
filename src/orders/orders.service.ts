58fb6a639dab3f563487acce71bdec9b3c5dafb7
Ilkin Mammadli <ilkinmammadli01@gmail.com>
2023-08-17 16:23:20 +0400

    refactor!: create order transaction method

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,143 +1,90 @@
-import { CACHE_MANAGER } from "@nestjs/cache-manager";
-import {
-  Inject,
-  Injectable, NotFoundException,
-  ServiceUnavailableException,
-} from "@nestjs/common";
-import { Order, Product } from "@prisma/client";
-import { Cache } from "cache-manager";
+import { Injectable, ServiceUnavailableException } from "@nestjs/common";
+import { Order, Prisma, Product } from "@prisma/client";
 import { PrismaService } from "../prisma/prisma.service";
 import { ProductsService } from "../products/products.service";
 import { UsersService } from "../users/users.service";
-import { CreateOrderDto, UpdateOrderDto } from "./dtos";
-
-type OrderData = Omit<CreateOrderDto, "products"> & {
-  userId: number,
-  productRecords: Product[]
-};
+import { CreateOrderDto } from "./dtos";
 
 @Injectable()
 export class OrdersService {
   constructor(
-    @Inject(CACHE_MANAGER) private readonly cacheManager: Cache,
     private readonly prismaService: PrismaService,
-    private readonly usersService: UsersService,
     private readonly productsService: ProductsService,
+    private readonly usersService: UsersService,
   ) {}
 
-  public async create(
+  public async createOrderTransaction(
     userId: number,
     createOrderDto: CreateOrderDto,
   ): Promise<Order> {
-    const {
-      products,
-      ...rest
-    }: CreateOrderDto = createOrderDto;
-    const productRecords: Product[] = await this.productsService.getProductsByIds(
-      products);
-    const orderData: OrderData = {
-      userId,
-      productRecords,
-      ...rest,
-    };
-    return this.prismaService.$transaction(
-      async (prisma: PrismaService): Promise<Order> => {
-        const createdOrder: Order = await this.createOrder(prisma, orderData);
-        if (!createdOrder) {
-          throw new ServiceUnavailableException(
-            "the transaction cannot be fulfilled");
-        }
+    const { products }: CreateOrderDto = createOrderDto;
+    return this.prismaService.$transaction(async (prisma: PrismaService): Promise<Order> => {
+      try {
+        const createdOrder: Order = await this.createOrder(
+          prisma,
+          userId,
+          createOrderDto,
+        );
+        const orderTotalPrice: number = await this.calculateOrderTotalPrice(
+          products);
+        await this.updateOrderTotalPrice(prisma, createdOrder.id, orderTotalPrice);
         await this.usersService.updateUserOrders(
           prisma,
           userId,
           createdOrder.id,
         );
+        await Promise.all(
+          products.map((productId: number) => prisma.productsOnOrder.create({
+            data: {
+              product: { connect: { id: productId } },
+              order: { connect: { id: createdOrder.id } },
+            },
+          })),
+        );
         return createdOrder;
-      });
-  }
-
-  public async createOrder(
-    prisma: Partial<PrismaService>,
-    orderData: OrderData,
-  ): Promise<Order> {
-    const {
-      userId,
-      productRecords,
-      ...rest
-    }: OrderData = orderData;
-    return prisma.order.create({
-      data: {
-        ...rest,
-        order_no: Date.now().toString(),
-        user: { connect: { id: userId } },
-        products: {
-          connect: productRecords.map((product: Product): {
-            id: number
-          } => (
-            { id: product.id }
-          )),
-        },
-      },
+      } catch (error) {
+        throw new ServiceUnavailableException("failed to create order");
+      }
     });
   }
 
-  public async orders(): Promise<Order[]> {
-    const cachedOrders: Order[] = await this.cacheManager.get<Order[]>("orders");
-    if (cachedOrders) {
-      return cachedOrders;
-    }
-    const orders: Order[] = await this.prismaService.order.findMany({});
-    await this.cacheManager.set("orders", orders, )
-    return orders
-  }
-
-  public async order(id: number): Promise<Order> {
-    const cachedOrder: Order = await this.cacheManager.get<Order>(`order-${id}`);
-    if (cachedOrder) {
-      return cachedOrder;
-    }
-    const order: Order = await this.prismaService.order.findUnique({
-      where: {
-        id,
-      },
-    });
-    if (!order) {
-      throw new NotFoundException("order not found!");
-    }
-    await this.cacheManager.set(`order-${id}`, order);
-    return order;
-  }
-
-  public async update(
-    id: number,
-    updateOrderDto: UpdateOrderDto,
+  private async createOrder(
+    prisma: PrismaService,
+    userId: number,
+    createOrderDto: CreateOrderDto,
   ): Promise<Order> {
     const {
       products,
-      ...rest
-    }: UpdateOrderDto = updateOrderDto;
-    const productRecords: Product[] = await this.productsService.getProductsByIds(
-      products);
-    return this.prismaService.order.update({
-      where: {
-        id,
-      },
-      data: {
-        ...rest,
-        products: {
-          connect: productRecords.map((product: Product): { id: number } => (
-            { id: product.id }
-          )),
-        },
-      },
+      ...createOrderData
+    }: CreateOrderDto = createOrderDto;
+    const orderCreateData: Prisma.OrderCreateInput = {
+      ...createOrderData,
+      order_no: Date.now().toString(),
+      user: { connect: { id: userId } },
+    };
+    return prisma.order.create({
+      data: orderCreateData,
     });
   }
 
-  public async delete(id: number): Promise<Order> {
-    return this.prismaService.order.delete({
-      where: {
-        id,
-      },
+  private async updateOrderTotalPrice(
+    prisma: PrismaService,
+    orderId: number,
+    totalPrice: number,
+  ): Promise<void> {
+    await prisma.order.update({
+      where: { id: orderId },
+      data: { total_price: totalPrice },
     });
   }
+
+  private async calculateOrderTotalPrice(productIds: number[]): Promise<number> {
+    const products: Product[] = await this.productsService.getProductsByIds(
+      productIds);
+    return products.reduce(
+      (totalPrice: number, product: Product) => totalPrice + product.price,
+      0,
+    );
+  }
+
 }

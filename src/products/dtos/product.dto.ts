5bab16ab55199c3ce23aa30851cfc348f9a27796
Ilkin Mammadli <ilkinmammadli01@gmail.com>
2023-08-17 10:26:43 +0400

    feat(docs): add docs for product dto

--- src/products/dtos/product.dto.ts
+++ src/products/dtos/product.dto.ts
@@ -1,15 +1,32 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { Expose } from "class-transformer";
 
 export class ProductDto {
+  @ApiProperty({
+    description: "Id of the product",
+    example: 1,
+  })
   @Expose()
   id: number;
 
+  @ApiProperty({
+    description: "Title of the product",
+    example: "T-shirt",
+  })
   @Expose()
   title: string;
 
+  @ApiProperty({
+    description: "Description of the product",
+    example: "Blue cotton T-shirt",
+  })
   @Expose()
   description: string;
 
+  @ApiProperty({
+    description: "Price of the product",
+    example: 20,
+  })
   @Expose()
   price: number;
 }

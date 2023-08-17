14d48873c384e81a7ab57c70e63113632941fc25
Ilkin Mammadli <ilkinmammadli01@gmail.com>
2023-08-17 16:23:36 +0400

    refactor!: add create order controller method

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,58 +1,22 @@
-import {
-  Body,
-  Controller, Delete, Get, Param, ParseIntPipe, Patch,
-  Post,
-  UseGuards,
-} from "@nestjs/common";
+import { Body, Controller, Post, UseGuards } from "@nestjs/common";
 import { Order } from "@prisma/client";
 import { CurrentUser } from "../common/decorators";
 import { JwtAuthGuard } from "../common/guards";
-import { CreateOrderDto, UpdateOrderDto } from "./dtos";
+import { CreateOrderDto } from "./dtos";
 import { OrdersService } from "./orders.service";
 
+@UseGuards(JwtAuthGuard)
 @Controller("orders")
 export class OrdersController {
   constructor(
     private readonly ordersService: OrdersService,
   ) {}
 
-  @UseGuards(JwtAuthGuard)
   @Post()
   public async createOrder(
-    @CurrentUser("sub") orderId: number,
-    @Body() createOrderDto: CreateOrderDto,
+    @CurrentUser("sub") userId: number,
+    @Body() createOrderDto: CreateOrderDto
   ): Promise<Order> {
-    return this.ordersService.create(orderId, createOrderDto);
-  }
-
-  @UseGuards(JwtAuthGuard)
-  @Get()
-  public async getOrders(): Promise<Order[]> {
-    return this.ordersService.orders();
-  }
-
-  @UseGuards(JwtAuthGuard)
-  @Get(":id")
-  public async getOrder(
-    @Param("id", ParseIntPipe) orderId: number,
-  ): Promise<Order> {
-    return this.ordersService.order(orderId);
-  }
-
-  @UseGuards(JwtAuthGuard)
-  @Patch(":id")
-  public async updateOrder(
-    @Param("id", ParseIntPipe) orderId: number,
-    @Body() updateOrderDto: UpdateOrderDto,
-  ): Promise<Order> {
-    return this.ordersService.update(orderId, updateOrderDto);
-  }
-
-  @UseGuards(JwtAuthGuard)
-  @Delete(":id")
-  public async deleteOrder(
-    @Param(":id", ParseIntPipe) orderId: number,
-  ): Promise<Order> {
-    return this.ordersService.delete(orderId);
+    return this.ordersService.createOrderTransaction(userId, createOrderDto);
   }
 }

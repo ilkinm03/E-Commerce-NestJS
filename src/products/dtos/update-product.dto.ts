2dd3d04427cd482c6754d7c175c8855facc50eb5
Ilkin Mammadli <ilkinmammadli01@gmail.com>
2023-08-31 11:48:40 +0400

    feat(docs): add docs for update product dto

--- src/products/dtos/update-product.dto.ts
+++ src/products/dtos/update-product.dto.ts
@@ -1,16 +1,34 @@
+import { ApiPropertyOptional } from "@nestjs/swagger";
 import { IsNumber, IsOptional, IsString, Length, Max } from "class-validator";
 
 export class UpdateProductDto {
+  @ApiPropertyOptional({
+    description: "New title of the product",
+    minLength: 5,
+    maxLength: 64,
+    example: "Updated title",
+  })
   @IsOptional()
   @IsString()
   @Length(5, 64)
   title?: string;
 
+  @ApiPropertyOptional({
+    description: "New Description of the product",
+    minLength: 5,
+    maxLength: 128,
+    example: "Updated description",
+  })
   @IsOptional()
   @IsString()
   @Length(5, 128)
   description?: string;
 
+  @ApiPropertyOptional({
+    description: "New price of the product",
+    maximum: 10000,
+    example: 20,
+  })
   @IsOptional()
   @IsNumber()
   @Max(10000)

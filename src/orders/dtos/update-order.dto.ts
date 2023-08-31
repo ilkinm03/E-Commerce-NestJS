bac4914fc5ec11ee5f56ccc28ae26468510ea71d
Ilkin Mammadli <ilkinmammadli01@gmail.com>
2023-08-31 16:12:27 +0400

    refactor(docs): add docs for update order dto

--- src/orders/dtos/update-order.dto.ts
+++ src/orders/dtos/update-order.dto.ts
@@ -1,10 +1,17 @@
+import { ApiPropertyOptional } from "@nestjs/swagger";
 import { IsOptional, IsString } from "class-validator";
 
 export class UpdateOrderDto {
+  @ApiPropertyOptional({
+    description: "Updated order payment method",
+  })
   @IsOptional()
   @IsString()
   payment_method?: string;
 
+  @ApiPropertyOptional({
+    description: "Updated order shipping method",
+  })
   @IsOptional()
   @IsString()
   shipping_method?: string;

00085090a49d2e4002d2384b0df1b0c2d8997925
Ilkin Mammadli <ilkinmammadli01@gmail.com>
2023-08-31 11:39:18 +0400

    feat(docs): add docs for create product dto

--- src/products/dtos/create-product.dto.ts
+++ src/products/dtos/create-product.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from "@nestjs/swagger";
 import {
   IsNotEmpty,
   IsNumber,
@@ -8,16 +9,33 @@ import {
 } from "class-validator";
 
 export class CreateProductDto {
+  @ApiProperty({
+    description: "Title of the product",
+    minLength: 5,
+    maxLength: 64,
+    example: "T-Shirt",
+  })
   @IsNotEmpty()
   @IsString()
   @Length(5, 64)
   title: string;
 
+  @ApiProperty({
+    description: "Description of the product",
+    minLength: 5,
+    maxLength: 128,
+    example: "Red S-size Men T-Shirt",
+  })
   @IsNotEmpty()
   @IsString()
   @Length(5, 128)
   description: string;
 
+  @ApiProperty({
+    description: "Price of the product",
+    maximum: 10000,
+    example: 20,
+  })
   @IsNotEmpty()
   @IsNumber()
   @Max(10000)

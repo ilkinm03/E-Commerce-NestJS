93a7d78e1e94e16c0cfac2ecc3ba32b170d0efa7
Ilkin Mammadli <ilkinmammadli01@gmail.com>
2023-09-15 09:06:32 +0400

    fix: provide users service to roles module

--- src/roles/roles.module.ts
+++ src/roles/roles.module.ts
@@ -1,10 +1,11 @@
 import { Module } from "@nestjs/common";
 import { PrismaService } from "../prisma/prisma.service";
+import { UsersService } from "../users/users.service";
 import { RolesController } from "./roles.controller";
 import { RolesService } from "./roles.service";
 
 @Module({
   controllers: [RolesController],
-  providers: [RolesService, PrismaService],
+  providers: [RolesService, PrismaService, UsersService],
 })
 export class RolesModule {}

70a9239df33dc08b8cbfbe411d5771facf88add2
Ilkin Mammadli <ilkinmammadli01@gmail.com>
2023-09-15 09:07:06 +0400

    chore: remove unnecessary permissions dependency

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,14 +1,13 @@
 import { Module } from "@nestjs/common";
 import { PrismaService } from "../prisma/prisma.service";
-import { PermissionsService } from "../roles/permissions.service";
 import { RolesService } from "../roles/roles.service";
 import { UsersController } from "./users.controller";
 import { UsersService } from "./users.service";
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService, RolesService, PermissionsService, PrismaService],
-  exports: [UsersService, RolesService, PermissionsService]
+  providers: [UsersService, RolesService, PrismaService],
+  exports: [UsersService, RolesService],
 })
 export class UsersModule {
 }

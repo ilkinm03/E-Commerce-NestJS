03bb8e68e98b390475a141877f114e774c9d6b7c
Ilkin Mammadli <ilkinmammadli01@gmail.com>
2023-09-15 09:04:12 +0400

    feat: add all the necessary env vars to config schema

--- src/config/config.schema.ts
+++ src/config/config.schema.ts
@@ -11,4 +11,10 @@ export class ConfigSchema {
   JWT_REFRESH_TOKEN_SECRET: Joi.StringSchema = Joi.string().required();
   JWT_ACCESS_TOKEN_EXP_TIME: Joi.NumberSchema = Joi.number().required();
   JWT_REFRESH_TOKEN_EXP_TIME: Joi.NumberSchema = Joi.number().required();
+  POSTGRES_USER: Joi.StringSchema = Joi.string().required();
+  POSTGRES_PASSWORD: Joi.StringSchema = Joi.string().required();
+  POSTGRES_DB: Joi.StringSchema = Joi.string().required();
+  GOOGLE_OAUTH_CLIENT_ID: Joi.StringSchema = Joi.string().required();
+  GOOGLE_OAUTH_CLIENT_SECRET: Joi.StringSchema = Joi.string().required();
+  GOOGLE_OAUTH_CALLBACK_URL: Joi.StringSchema = Joi.string().required();
 }
